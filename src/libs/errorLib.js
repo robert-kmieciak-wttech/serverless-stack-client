a3f2f29367b8e246a21f4e0f0218ae7de8f653a4
Robert Kmieciak <robert.kmieciak@wundermanthompson.com>
2021-05-13 21:01:31 +0200

    error handling

--- src/libs/errorLib.js
+++ src/libs/errorLib.js
@@ -1,10 +1,43 @@
-export function onError(error) {
-    let message = error.toString();
+import * as Sentry from "@sentry/browser";
 
-    // Auth errors
-    if (!(error instanceof Error) && error.message) {
-      message = error.message;
-    }
+const isLocal = process.env.NODE_ENV === "development";
 
-    alert(message);
+export function initSentry() {
+  if (isLocal) {
+    return;
   }
+
+  Sentry.init({
+    dsn:
+      "https://3f66c17efb024e0d9efe2ece56e1afb1@o657697.ingest.sentry.io/5763291"});
+}
+
+export function logError(error, errorInfo = null) {
+  if (isLocal) {
+    return;
+  }
+
+  Sentry.withScope((scope) => {
+    errorInfo && scope.setExtras(errorInfo);
+    Sentry.captureException(error);
+  });
+}
+
+export function onError(error) {
+  let errorInfo = {};
+  let message = error.toString();
+
+  // Auth errors
+  if (!(error instanceof Error) && error.message) {
+    errorInfo = error;
+    message = error.message;
+    error = new Error(message);
+    // API errors
+  } else if (error.config && error.config.url) {
+    errorInfo.url = error.config.url;
+  }
+
+  logError(error, errorInfo);
+
+  alert(message);
+}
